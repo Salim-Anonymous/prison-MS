371d8952a78e665eea084ca99a6066d168910806
salim <02200163.cst@rub.edu.bt>
2022-12-21 18:23:51 +0600

    initial Project setup

---
+++ prisma/migrations/20221213081336_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "SuperIntendent" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "deletedAt" DATETIME
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "SuperIntendent_email_key" ON "SuperIntendent"("email");
