589e2e9ed3b350b51eaa8315c103dadd026b174e
penjorkinley <penjorkinley77@gmail.com>
2022-12-31 13:57:08 +0600

    update index.js

--- client/src/Pages/inmate_details/index.jsx
+++ client/src/Pages/inmate_details/index.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
-export default function Inmates(){
-    return (
-        <div>
-            <h1>inmate details</h1>
-        </div>
-    )
+export default function Inmates() {
+  return (
+    <div>
+      <h1>inmate details</h1>
+    </div>
+  );
 }
