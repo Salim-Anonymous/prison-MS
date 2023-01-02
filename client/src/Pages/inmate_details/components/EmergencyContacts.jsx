fe792aa14f8921b0ad860199afd95ad3f1f47bc9
penjorkinley <penjorkinley77@gmail.com>
2023-01-02 16:32:50 +0600

    Update Emergency contact

--- client/src/Pages/inmate_details/components/EmergencyContacts.jsx
+++ client/src/Pages/inmate_details/components/EmergencyContacts.jsx
@@ -5,17 +5,13 @@ const EmergencyContacts = () => {
     <div>
       <h4 className="text-lg font-medium p-2 mt-5">Emergency Contact</h4>
       <div className="flex flex-row p-2 ">
-        <div className="flex flex-col w-[70%] mr-6">
+        <div className="flex flex-col w-1/3 mr-6">
           <label className="text-base font-medium">Name</label>
           <input
             type="text"
             className="border-2 border-[#00437A] mt-2 focus:outline-none p-1 w-full "
           />
         </div>
-        <div className="flex flex-col w-1/3 "></div>
-      </div>
-
-      <div className="flex flex-row p-2 ">
         <div className="flex flex-col w-1/3 mr-6">
           <label className="text-base font-medium">Contact Number</label>
           <input
@@ -23,14 +19,13 @@ const EmergencyContacts = () => {
             className="border-2 border-[#00437A] mt-2 focus:outline-none p-1 w-full"
           />
         </div>
-        <div className="flex flex-col w-1/3 mr-6">
+        <div className="flex flex-col w-1/3 ">
           <label className="text-base font-medium">Relation</label>
           <input
             type="text"
             className="border-2 border-[#00437A] mt-2 focus:outline-none p-1 w-full"
           />
         </div>
-        <div className="flex flex-col w-1/3 "></div>
       </div>
     </div>
   );
