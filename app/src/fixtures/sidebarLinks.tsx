21d88f541f5735d58cb8be2e286c55cafe29740e
Salim <02200163.cst@rub.edu.bt>
2023-03-05 19:12:25 +0530

    a lot of things

---
+++ app/src/fixtures/sidebarLinks.tsx
@@ -0,0 +1,20 @@
+const sidebarLinks = [
+    {
+        title: "Dashboard",
+        link: "/dashboard"
+    },
+    {
+        title: "Prison Act 2009",
+        link: "/prison-act-2009"
+    },
+    {
+        title: "Inmates",
+        link: "/inmates"
+    },
+    {
+        title: "Prison",
+        link: "/prison"
+    },
+    {
+        title: ""
+    }
